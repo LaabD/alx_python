fa11f9107a3fa105622fed64e7658bfe78fba479
Dennis <laabso2013@gmail.com>
2023-07-31 18:37:41 +0000

    The last digit

--- alx_python/python-hello_world/2-positive_or_negative.py
+++ alx_python/python-hello_world/2-positive_or_negative.py
@@ -4,8 +4,11 @@ number = random.randint(-10, 10)
 # YOUR CODE HERE
 if number > 0:
     print("is positive")
+    print()
 elif number < 0:
     print("is negative")
+    print()
 else:
     print("it zero")
+    print()
 
